f1fa15e031582c30fcf292bb1d9906b087b882d6
DELL <DELL@DESKTOP-HNTF6V8>
2022-07-10 01:19:41 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimplePagination.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
